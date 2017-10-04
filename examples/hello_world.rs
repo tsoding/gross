246acbb098cb916aa2990d8e99a561dacbf2a50c
rexim <reximkut@gmail.com>
2017-10-04 23:38:58 +0700

    Implement skeleton of gross::simulate

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,5 +1,18 @@
 extern crate gross;
 
+struct State {
+    x: i32,
+    y: i32
+}
+
 fn main() {
-    gross::hello_world();
+    gross::simulate(gross::Display::FullScreen,
+                    gross::Color::RGB(0, 0, 0),
+                    30,
+                    State { x: 0, y: 0 },
+                    |ref s| gross::Picture::Blank,
+                    |mut s| {
+                        s.x = (s.x + 10) % 100;
+                        s
+                    })
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,31 @@
-pub fn hello_world() {
-    println!("Fuck you!")
+pub enum Color {
+    RGB(u8, u8, u8)
+}
+
+pub enum Display {
+    FullScreen,
+    InWindow(String, (i32, i32), (u32, u32))
+}
+
+pub enum Picture {
+    Blank
+}
+
+pub fn simulate<S, R, U>(display: Display,
+                         background_color: Color,
+                         fps: u32,
+                         init_state: S,
+                         render: R,
+                         update: U)
+    where R: Fn(&S) -> Picture,
+          U: Fn(S) -> S {
+
+    let mut state = init_state;
+    loop {
+        let picture = render(&state);
+        // ...actually render the state somehow...
+        state = update(state);
+    }
 }
 
 #[cfg(test)]
