63b2bfb69061e8da0f5b95747efb0f5cc049a3dd
rexim <reximkut@gmail.com>
2017-10-08 01:16:47 +0700

    Implement readability of Hello World example (#8)

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,15 +1,19 @@
 extern crate gross;
 
+use gross::*;
+
 struct State {
     x: i32,
     y: i32
 }
 
 fn main() {
+    use Picture::*;
+
     gross::simulate(gross::Display::InWindow((10, 10), (800, 600)),
                     State { x: 0, y: 0 },
-                    |ref s| gross::Picture::Pictures(vec![gross::Picture::Rectangle(s.x, s.y, 100, 100),
-                                                          gross::Picture::Line(s.x, s.y, 800, 600)]),
+                    |ref s| Pictures(vec![Rectangle(s.x, s.y, 100, 100),
+                                          Line(s.x, s.y, 800, 600)]),
                     |mut s| {
                         s.x = (s.x + 1) % 800;
                         s
